c48436d3b20c13f51eae26059d7c8b2b538220fc
maklinm <monuzhin@gmail.com>
2025-12-06 15:56:21 +0300

    Add files via upload

---
+++ ProbnGameForm/ProbnGameForm/Form1.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace ProbnGameForm
+{
+    partial class Probn
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttM = new System.Windows.Forms.Button();
+            this.buttD = new System.Windows.Forms.Button();
+            this.tumb1 = new System.Windows.Forms.Button();
+            this.tumb2 = new System.Windows.Forms.Button();
+            this.knopS = new System.Windows.Forms.Button();
+            this.knopSN = new System.Windows.Forms.Button();
+            this.knopP = new System.Windows.Forms.Button();
+            this.knopPN = new System.Windows.Forms.Button();
+            this.lampa = new System.Windows.Forms.PictureBox();
+            this.lampa2 = new System.Windows.Forms.PictureBox();
+            this.lamp3_1 = new System.Windows.Forms.PictureBox();
+            this.Pacet1 = new System.Windows.Forms.Button();
+            this.Pacet2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.lampa)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lampa2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lamp3_1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttM
+            // 
+            this.buttM.BackColor = System.Drawing.Color.Transparent;
+            this.buttM.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttM.FlatAppearance.BorderSize = 0;
+            this.buttM.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.buttM.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.buttM.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttM.ForeColor = System.Drawing.Color.Transparent;
+            this.buttM.Image = global::ProbnGameForm.Properties.Resources.TumM;
+            this.buttM.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.buttM.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.buttM.Location = new System.Drawing.Point(301, 560);
+            this.buttM.Name = "buttM";
+            this.buttM.Size = new System.Drawing.Size(77, 60);
+            this.buttM.TabIndex = 2;
+            this.buttM.UseVisualStyleBackColor = false;
+            this.buttM.Click += new System.EventHandler(this.buttM_Click_1);
+            // 
+            // buttD
+            // 
+            this.buttD.BackColor = System.Drawing.Color.Transparent;
+            this.buttD.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttD.FlatAppearance.BorderSize = 0;
+            this.buttD.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.buttD.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.buttD.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttD.ForeColor = System.Drawing.Color.Transparent;
+            this.buttD.Image = global::ProbnGameForm.Properties.Resources.TumD;
+            this.buttD.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.buttD.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.buttD.Location = new System.Drawing.Point(301, 560);
+            this.buttD.Name = "buttD";
+            this.buttD.Size = new System.Drawing.Size(77, 60);
+            this.buttD.TabIndex = 3;
+            this.buttD.UseVisualStyleBackColor = false;
+            this.buttD.Click += new System.EventHandler(this.buttD_Click);
+            // 
+            // tumb1
+            // 
+            this.tumb1.BackColor = System.Drawing.Color.Transparent;
+            this.tumb1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.tumb1.FlatAppearance.BorderSize = 0;
+            this.tumb1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.tumb1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.tumb1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.tumb1.ForeColor = System.Drawing.Color.Transparent;
+            this.tumb1.Image = global::ProbnGameForm.Properties.Resources.tumb11;
+            this.tumb1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.tumb1.Location = new System.Drawing.Point(98, 331);
+            this.tumb1.Name = "tumb1";
+            this.tumb1.Size = new System.Drawing.Size(66, 84);
+            this.tumb1.TabIndex = 4;
+            this.tumb1.UseVisualStyleBackColor = false;
+            this.tumb1.Click += new System.EventHandler(this.tumb1_Click_1);
+            // 
+            // tumb2
+            // 
+            this.tumb2.BackColor = System.Drawing.Color.Transparent;
+            this.tumb2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.tumb2.FlatAppearance.BorderSize = 0;
+            this.tumb2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.tumb2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.tumb2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.tumb2.ForeColor = System.Drawing.Color.Transparent;
+            this.tumb2.Image = global::ProbnGameForm.Properties.Resources.tumb22;
+            this.tumb2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.tumb2.Location = new System.Drawing.Point(98, 331);
+            this.tumb2.Name = "tumb2";
+            this.tumb2.Size = new System.Drawing.Size(66, 84);
+            this.tumb2.TabIndex = 5;
+            this.tumb2.UseVisualStyleBackColor = false;
+            this.tumb2.Click += new System.EventHandler(this.tumb2_Click_1);
+            // 
+            // knopS
+            // 
+            this.knopS.BackColor = System.Drawing.Color.Transparent;
+            this.knopS.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.knopS.FlatAppearance.BorderSize = 0;
+            this.knopS.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.knopS.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.knopS.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.knopS.Image = global::ProbnGameForm.Properties.Resources.knopka1;
+            this.knopS.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.knopS.Location = new System.Drawing.Point(384, 560);
+            this.knopS.Name = "knopS";
+            this.knopS.Size = new System.Drawing.Size(34, 35);
+            this.knopS.TabIndex = 7;
+            this.knopS.UseVisualStyleBackColor = false;
+            this.knopS.Click += new System.EventHandler(this.knopS_Click);
+            // 
+            // knopSN
+            // 
+            this.knopSN.BackColor = System.Drawing.Color.Transparent;
+            this.knopSN.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.knopSN.FlatAppearance.BorderSize = 0;
+            this.knopSN.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.knopSN.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.knopSN.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.knopSN.Image = global::ProbnGameForm.Properties.Resources.knopka11;
+            this.knopSN.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.knopSN.Location = new System.Drawing.Point(384, 560);
+            this.knopSN.Name = "knopSN";
+            this.knopSN.Size = new System.Drawing.Size(34, 35);
+            this.knopSN.TabIndex = 8;
+            this.knopSN.UseVisualStyleBackColor = false;
+            this.knopSN.Click += new System.EventHandler(this.knopSN_Click_1);
+            // 
+            // knopP
+            // 
+            this.knopP.BackColor = System.Drawing.Color.Transparent;
+            this.knopP.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.knopP.FlatAppearance.BorderSize = 0;
+            this.knopP.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.knopP.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.knopP.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.knopP.Image = global::ProbnGameForm.Properties.Resources.knopka2;
+            this.knopP.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.knopP.Location = new System.Drawing.Point(444, 560);
+            this.knopP.Name = "knopP";
+            this.knopP.Size = new System.Drawing.Size(38, 35);
+            this.knopP.TabIndex = 9;
+            this.knopP.UseVisualStyleBackColor = false;
+            this.knopP.Click += new System.EventHandler(this.knopP_Click_1);
+            // 
+            // knopPN
+            // 
+            this.knopPN.BackColor = System.Drawing.Color.Transparent;
+            this.knopPN.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.knopPN.FlatAppearance.BorderSize = 0;
+            this.knopPN.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.knopPN.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.knopPN.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.knopPN.Image = global::ProbnGameForm.Properties.Resources.knopka22;
+            this.knopPN.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.knopPN.Location = new System.Drawing.Point(444, 560);
+            this.knopPN.Name = "knopPN";
+            this.knopPN.Size = new System.Drawing.Size(38, 35);
+            this.knopPN.TabIndex = 10;
+            this.knopPN.UseVisualStyleBackColor = false;
+            this.knopPN.Click += new System.EventHandler(this.knopPN_Click_1);
+            // 
+            // lampa
+            // 
+            this.lampa.BackColor = System.Drawing.Color.Transparent;
+            this.lampa.Image = global::ProbnGameForm.Properties.Resources.lampo1;
+            this.lampa.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.lampa.Location = new System.Drawing.Point(453, 509);
+            this.lampa.Name = "lampa";
+            this.lampa.Size = new System.Drawing.Size(29, 27);
+            this.lampa.TabIndex = 11;
+            this.lampa.TabStop = false;
+            this.lampa.UseWaitCursor = true;
+            this.lampa.Visible = false;
+            // 
+            // lampa2
+            // 
+            this.lampa2.BackColor = System.Drawing.Color.Transparent;
+            this.lampa2.Image = global::ProbnGameForm.Properties.Resources.lampo21;
+            this.lampa2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.lampa2.Location = new System.Drawing.Point(394, 509);
+            this.lampa2.Name = "lampa2";
+            this.lampa2.Size = new System.Drawing.Size(33, 25);
+            this.lampa2.TabIndex = 12;
+            this.lampa2.TabStop = false;
+            this.lampa2.Visible = false;
+            // 
+            // lamp3_1
+            // 
+            this.lamp3_1.BackColor = System.Drawing.Color.Transparent;
+            this.lamp3_1.Image = global::ProbnGameForm.Properties.Resources.lampo31;
+            this.lamp3_1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.lamp3_1.Location = new System.Drawing.Point(394, 509);
+            this.lamp3_1.Name = "lamp3_1";
+            this.lamp3_1.Size = new System.Drawing.Size(33, 25);
+            this.lamp3_1.TabIndex = 13;
+            this.lamp3_1.TabStop = false;
+            this.lamp3_1.Visible = false;
+            // 
+            // Pacet1
+            // 
+            this.Pacet1.BackColor = System.Drawing.Color.Transparent;
+            this.Pacet1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Pacet1.FlatAppearance.BorderSize = 0;
+            this.Pacet1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.Pacet1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.Pacet1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Pacet1.ForeColor = System.Drawing.Color.Transparent;
+            this.Pacet1.Image = global::ProbnGameForm.Properties.Resources.Pacet1;
+            this.Pacet1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.Pacet1.Location = new System.Drawing.Point(655, 165);
+            this.Pacet1.Name = "Pacet1";
+            this.Pacet1.Size = new System.Drawing.Size(118, 36);
+            this.Pacet1.TabIndex = 14;
+            this.Pacet1.UseVisualStyleBackColor = false;
+            this.Pacet1.Click += new System.EventHandler(this.Pacet1_Click_1);
+            // 
+            // Pacet2
+            // 
+            this.Pacet2.BackColor = System.Drawing.Color.Transparent;
+            this.Pacet2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Pacet2.FlatAppearance.BorderSize = 0;
+            this.Pacet2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.Pacet2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.Pacet2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Pacet2.Image = global::ProbnGameForm.Properties.Resources.Pacet2;
+            this.Pacet2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.Pacet2.Location = new System.Drawing.Point(655, 165);
+            this.Pacet2.Name = "Pacet2";
+            this.Pacet2.Size = new System.Drawing.Size(118, 36);
+            this.Pacet2.TabIndex = 15;
+            this.Pacet2.UseVisualStyleBackColor = false;
+            this.Pacet2.Click += new System.EventHandler(this.Pacet2_Click_1);
+            // 
+            // Probn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::ProbnGameForm.Properties.Resources.Пульт_122;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1122, 759);
+            this.Controls.Add(this.Pacet1);
+            this.Controls.Add(this.Pacet2);
+            this.Controls.Add(this.lampa2);
+            this.Controls.Add(this.knopP);
+            this.Controls.Add(this.knopS);
+            this.Controls.Add(this.lamp3_1);
+            this.Controls.Add(this.lampa);
+            this.Controls.Add(this.knopPN);
+            this.Controls.Add(this.knopSN);
+            this.Controls.Add(this.tumb2);
+            this.Controls.Add(this.tumb1);
+            this.Controls.Add(this.buttM);
+            this.Controls.Add(this.buttD);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Probn";
+            this.ShowIcon = false;
+            this.Text = "Пробный запуск";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.lampa)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lampa2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lamp3_1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttM;
+        private System.Windows.Forms.Button buttD;
+        private System.Windows.Forms.Button tumb1;
+        private System.Windows.Forms.Button tumb2;
+        private System.Windows.Forms.Button knopS;
+        private System.Windows.Forms.Button knopSN;
+        private System.Windows.Forms.Button knopP;
+        private System.Windows.Forms.Button knopPN;
+        private System.Windows.Forms.PictureBox lampa;
+        private System.Windows.Forms.PictureBox lampa2;
+        private System.Windows.Forms.PictureBox lamp3_1;
+        private System.Windows.Forms.Button Pacet1;
+        private System.Windows.Forms.Button Pacet2;
+    }
+}
+
---
+++ ProbnGameForm/ProbnGameForm/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TrackBar;
+
+namespace ProbnGameForm
+{
+    public partial class Probn : Form
+    {
+        public Probn()
+        {
+            InitializeComponent();
+            MaximizeBox= false;
+        }
+               
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "Пробный запуск";
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+        }
+        
+
+
+
+
+
+        private void buttM_Click_1(object sender, EventArgs e)
+        {
+            buttM.Visible = false;
+        }
+
+        private void buttD_Click(object sender, EventArgs e)
+        {
+            buttM.Visible = true;
+        }
+
+        private void tumb1_Click_1(object sender, EventArgs e)
+        {
+            tumb2.Visible = true;
+        }
+
+        private void tumb2_Click_1(object sender, EventArgs e)
+        {
+            tumb2.Visible = false;
+        }
+
+        private void Pacet1_Click_1(object sender, EventArgs e)
+        {
+            Pacet1.Visible = false;
+        }
+
+        private void Pacet2_Click_1(object sender, EventArgs e)
+        {
+            Pacet1.Visible = true;
+        }
+
+        private void knopSN_Click_1(object sender, EventArgs e)
+        {
+            knopS.Visible = true;
+            if (knopS.Visible == true)
+            {
+                lampa2.Visible = false;
+                lamp3_1.Visible = true;
+
+                lamp3_1.Show();
+            }
+        }
+
+        private void knopS_Click(object sender, EventArgs e)
+        {
+            knopS.Visible = false;
+            if (knopS.Visible == false)
+            {
+                lamp3_1.Visible = false;
+            }
+        }
+
+        private void knopPN_Click_1(object sender, EventArgs e)
+        {
+            knopP.Visible = true;
+            if (knopP.Visible == true)
+            {
+                lampa.Visible = false;
+                lampa2.Visible = true;
+
+            }
+        }
+
+        private void knopP_Click_1(object sender, EventArgs e)
+        {
+            knopP.Visible = false;
+            if (knopP.Visible == false)
+            {
+                lampa2.Visible = false;
+                lampa.Visible = true;
+            }
+        }
+    }
+}
+
+ 
